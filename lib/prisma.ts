e291bfcc2b4b222d0bfb171696e1eef080c2b1db
mabdullah356 <abdullahworld111@gmail.com>
2026-07-16 23:41:32 +0500

    refector : fix the connnection issue in PrismaClient using pg-adapter

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,13 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
 const globalForPrisma = global as {
     prisma?: PrismaClient;
 };
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient({
-    accelerateUrl: process.env.DATABASE_URL,
-});
+function createPrismaClient() {
+    const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
+    return new PrismaClient({ adapter });
+}
+
+export const prisma = globalForPrisma.prisma ?? createPrismaClient();
